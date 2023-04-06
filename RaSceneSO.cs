8940b03c5227ebd36e92e740823a87ddfd8e8739
DESKTOP-3T4A7P1\Ramses <ik_ben_ramses@hotmail.com>
2023-04-06 21:59:33 +0200

    Corrected formatting

--- RaSceneSO.cs
+++ RaSceneSO.cs
@@ -4,46 +4,46 @@ using UnityEngine.SceneManagement;
 namespace RaScenesSO
 {
 	public class RaSceneSO : ScriptableObject
+	{
+		[SerializeField]
+		private string _nickname = string.Empty;
+
+		[SerializeField]
+		private SceneReference _sceneReference;
+
+		[SerializeField, HideInInspector]
+		private string _sceneName = string.Empty;
+
+		#region Properties
+
+		public string Nickname => string.IsNullOrEmpty(_nickname) ? name : _nickname;
+
+		public string SceneName => _sceneName;
+
+		public string ScenePath => _sceneReference.GetScenePath();
+
+		#endregion
+
+		#region Public Methods
+
+		/// <summary>
+		/// This method only works if the scene is currently loaded in
+		/// </summary>
+		public Scene GetUnitySceneData()
 		{
-			[SerializeField]
-			private string _nickname = string.Empty;
+			return SceneManager.GetSceneByName(SceneName);
+		}
 
-			[SerializeField]
-			private SceneReference _sceneReference;
+		#endregion
 
-			[SerializeField, HideInInspector]
-			private string _sceneName = string.Empty;
+		#region Protected Methods
 
-			#region Properties
+		protected void OnValidate()
+		{
+			_sceneName = _sceneReference.GetSceneName();
+		}
 
-			public string Nickname => string.IsNullOrEmpty(_nickname) ? name : _nickname;
+		#endregion
 
-			public string SceneName => _sceneName;
-
-			public string ScenePath => _sceneReference.GetScenePath();
-
-			#endregion
-
-			#region Public Methods
-
-			/// <summary>
-			/// This method only works if the scene is currently loaded in
-			/// </summary>
-			public Scene GetUnitySceneData()
-			{
-				return SceneManager.GetSceneByName(SceneName);
-			}
-
-			#endregion
-
-			#region Protected Methods
-
-			protected void OnValidate()
-			{
-				_sceneName = _sceneReference.GetSceneName();
-			}
-
-			#endregion
-		
 	}
 }
--- SceneReference.cs
+++ SceneReference.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 namespace RaScenesSO
 {
 	[System.Serializable]
-		public struct SceneReference
+	public struct SceneReference
+	{
+		#region Editor Variables
+
+		[SerializeField]
+		private string _scenePath;
+
+		#endregion
+
+		#region Public Methods
+
+		public string GetSceneName()
 		{
-			#region Editor Variables
-
-			[SerializeField]
-			private string _scenePath;
-
-			#endregion
-
-			#region Public Methods
-
-			public string GetSceneName()
-			{
-				return Path.GetFileNameWithoutExtension(_scenePath);
-			}
-
-			public string GetScenePath()
-			{
-				return _scenePath;
-			}
-
-			public bool IsValid()
-			{
-				return !string.IsNullOrEmpty(_scenePath);
-			}
-
-			#endregion
+			return Path.GetFileNameWithoutExtension(_scenePath);
 		}
+
+		public string GetScenePath()
+		{
+			return _scenePath;
+		}
+
+		public bool IsValid()
+		{
+			return !string.IsNullOrEmpty(_scenePath);
+		}
+
+		#endregion
+	}
 }
